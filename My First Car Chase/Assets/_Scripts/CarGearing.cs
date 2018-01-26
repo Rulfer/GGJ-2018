05e158e9a8cf5fb71c85627b50e5f99e581d6274
Øyvind Heddeland Instefjord <oheddela@cisco.com>
2018-01-26 22:38:50 +0100

    Take damage every second based on diff between currentGear and optimalGear

--- My First Car Chase/Assets/_Scripts/CarGearing.cs
+++ My First Car Chase/Assets/_Scripts/CarGearing.cs
@@ -10,6 +10,7 @@ public class CarGearing : MonoBehaviour
     public int optimalGear = 1;
 
     public int health = 100;
+    private bool takingDamage = false;
 
     private Speed speed;
 
@@ -17,34 +18,48 @@ public class CarGearing : MonoBehaviour
 	void Start ()
     {
         speed = (Speed) GameObject.Find("GameManager").GetComponent(typeof(Speed));
+        StartCoroutine(DamageRoutine());
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         optimalGear = speed.currentSpeed / speed.speedStep;
-        if (Input.GetKeyDown("w") && currentGear < maxGear)
-        {
+        if (Input.GetKeyDown("w"))
             currentGear += 1;
-        }
-        else if(Input.GetKeyDown("s") && currentGear > minGear)
-        {
+        else if(Input.GetKeyDown("s"))
             currentGear -= 1;
-        }
-        if(currentGear != optimalGear)
+        if (currentGear > maxGear)
+            currentGear = maxGear;
+        else if (currentGear < minGear)
+            currentGear = minGear;
+
+        if(currentGear == optimalGear)
+            takingDamage = false;
+        else if(currentGear != optimalGear)
+            takingDamage = true;
+    }
+
+    IEnumerator DamageRoutine()
+    {
+        while (true)
         {
-            TakeDamage();
+            yield return new WaitForSeconds(1);
+            if (takingDamage)
+            {
+                TakeDamage();
+            }
         }
-	}
+    }
 
     private void TakeDamage()
     {
         int damage = 0;
-        if(currentGear > optimalGear)
+        if (currentGear > optimalGear)
         {
             damage = (currentGear - optimalGear) * 2;
         }
-        else if(currentGear < optimalGear)
+        else if (currentGear < optimalGear)
         {
             damage = (optimalGear - currentGear) * 2;
         }
