c6cec63a1473a83464c9866c6c29ab937b0dbcd0
Bård Harald <lol_controll@hotmail.com>
2018-01-26 20:52:55 +0100

    Scoretext

---
+++ My First Car Chase/Assets/Score.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public Text scoreText;
+    private int points = 0;
+    public float timeUntilNewPoint;
+
+
+    private void Start()
+    {
+        StartCoroutine(PointGenerator());
+    }
+
+    private IEnumerator PointGenerator()
+    {
+        yield return new WaitForSeconds(timeUntilNewPoint);
+
+        points++;
+        scoreText.text = points.ToString();
+
+        StartCoroutine(PointGenerator());
+    }
+}
