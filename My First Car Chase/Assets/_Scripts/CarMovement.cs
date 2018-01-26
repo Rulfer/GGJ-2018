24881abde6008121cd655e08a5d360163b1dcc80
Øyvind Heddeland Instefjord <oheddela@cisco.com>
2018-01-26 21:23:23 +0100

    Remove debugs

--- My First Car Chase/Assets/_Scripts/CarMovement.cs
+++ My First Car Chase/Assets/_Scripts/CarMovement.cs
@@ -23,21 +23,19 @@ public class CarMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        Debug.Log("Is moving: " + moving);
         if (moving && player.transform.position == lanes[currentLaneIndex].transform.position)
             moving = false;
 
         int newLane = currentLaneIndex;
+
         float axis = Input.GetAxis("Horizontal");
-        Debug.Log(axis);
         if (axis < -0.1f)
             newLane += 1;
         else if (axis > 0.1f)
             newLane -= 1;
-        Debug.Log("New Lane: " + newLane);
+
         if (!moving && newLane != currentLaneIndex && -1 < newLane && newLane < lanes.Length)
         {
-            Debug.Log("Should move");
             currentLaneIndex = newLane;
             MovePlayer();
         }
