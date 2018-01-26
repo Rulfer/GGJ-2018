d75606b056e1da379f0108bec99ed47ce99bec44
Øyvind Heddeland Instefjord <oheddela@cisco.com>
2018-01-26 22:22:10 +0100

    Gearing prototype

---
+++ My First Car Chase/Assets/_Scripts/CarGearing.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarGearing : MonoBehaviour
+{
+    public int maxGear = 5;
+    public int minGear = 1;
+    public int currentGear = 1;
+    public int optimalGear = 1;
+
+    public int health = 100;
+
+    private Speed speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        speed = (Speed) GameObject.Find("GameManager").GetComponent(typeof(Speed));
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        optimalGear = speed.currentSpeed / speed.speedStep;
+        if (Input.GetKeyDown("w") && currentGear < maxGear)
+        {
+            currentGear += 1;
+        }
+        else if(Input.GetKeyDown("s") && currentGear > minGear)
+        {
+            currentGear -= 1;
+        }
+        if(currentGear != optimalGear)
+        {
+            TakeDamage();
+        }
+	}
+
+    private void TakeDamage()
+    {
+        int damage = 0;
+        if(currentGear > optimalGear)
+        {
+            damage = (currentGear - optimalGear) * 2;
+        }
+        else if(currentGear < optimalGear)
+        {
+            damage = (optimalGear - currentGear) * 2;
+        }
+        health -= damage;
+        Debug.Log("TAKING DAMAGE. CURRENT HEALTH: " + health);
+    }
+}
---
+++ My First Car Chase/Assets/_Scripts/Speed.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speed : MonoBehaviour
+{
+    public int speedStep = 20;
+    public int currentSpeed;
+    public int maxSpeed;
+
+    private CarGearing carGearing;
+
+	// Use this for initialization
+	void Start ()
+    {
+        currentSpeed = speedStep;
+        carGearing = (CarGearing) GameObject.FindGameObjectWithTag("Player").GetComponent(typeof(CarGearing));
+        maxSpeed = speedStep * carGearing.maxGear;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown("3"))
+        {
+            currentSpeed += speedStep;
+        }
+        else if (Input.GetKeyDown("2"))
+        {
+            currentSpeed -= speedStep;
+        }
+	}
+}
